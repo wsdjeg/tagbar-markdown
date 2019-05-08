e2183fb381a0ecea993445c944bb70b069b0f9c3
Wang Shidong <wsdjeg@outlook.com>
2019-05-08 16:07:29 +0800

    Update mdtags.lua

--- bin/mdtags.lua
+++ bin/mdtags.lua
@@ -1,1 +1,11 @@
 
+if 1
+    then
+    print("!_TAG_FILE_FORMAT 2 /extended format; --format=1 will not append ;\" to lines/' ")
+    print("!_TAG_FILE_SORTED 0 /0=unsorted, 1=sorted, 2=foldcase/' ")
+    print("!_TAG_PROGRAM_AUTHOR")
+    print("lvht /git@lvht.net/'")
+    print("!_TAG_PROGRAM_NAME mdctags //' ")
+    print("!_TAG_PROGRAM_URL https://github.com/lvht/tagbar-markdown /official site/'")
+    print("!_TAG_PROGRAM_VERSION 0.1.0 //'")
+end
